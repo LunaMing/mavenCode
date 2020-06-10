5d6bab4ff02c38df51fec8ff7dff160a18ac8139
Yueming Liu <44162157+LunaMing@users.noreply.github.com>
2020-06-11 00:11:20 +0800

    粘贴了另外两个文件

--- src/main/java/hadoopPack/WholeFileInputFormat.java
+++ src/main/java/hadoopPack/WholeFileInputFormat.java
@@ -1,4 +1,31 @@
 package hadoopPack;
 
-public class WholeFileInputFormat {
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+import java.io.IOException;
+
+public class WholeFileInputFormat extends FileInputFormat<Text, BytesWritable> {
+    protected boolean isSplitable(JobContext context, Path filename) {
+        return false;
+    }
+
+    public SingleFileNameReader createRecordReader(
+            InputSplit split, TaskAttemptContext context)throws IOException,InterruptedException{
+        return new SingleFileNameReader((FileSplit)split, context.getConfiguration());
+    }
+
+    @Override
+    public RecordReader<Text, BytesWritable> getRecordReader(InputSplit arg0, JobConf arg1, Reporter arg2)
+            throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
